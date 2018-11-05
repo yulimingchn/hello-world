2761b4bc2aca3cefec26abbcfdaa2918712d9238
v_yuliming <yuliming@chinasoftinfo.com>
2018-11-05 22:27:46 +0800

    购物车优化

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var app = new VTTCue({
+var app = new Vue({
     el:'#app',
     data:{
+        arr:[],
         list:[
             {
                 id:1,
@@ -20,19 +21,50 @@ var app = new VTTCue({
                 price : 21488,
                 count: 1
             }
-        ]
+        ],
+        checked:[],
+        checkall:false
     },
     computed : {
         totalPrice : function(){
             var total = 0;
             for (var i=0;i <this.list.length;i++){
                 var item = this.list[i];
-                total += item.price * item.count;
+                for(var j=0;j<this.checked.length;j++){
+                    if(item.id === this.checked[j]){
+                        total += item.price * item.count;
+                    }
+                }
             }
-            return total.toString.replace(/\B(?=(\d{3})+$)/g,',');
+            return total.toString().replace(/\B(?=(\d{3})+$)/g,',');
         }
     },
     methods:{
-
+            handleReduce : function(index){
+                    if (this.list[index].count ===1 ) return;
+                    this.list[index].count--;                
+                },
+            handleAdd : function(index){
+                    this.list[index].count ++ ;
+            },
+            handleRemove : function (index){ 
+                this.list.splice(index,1);
+            },
+            checkAll : function(){
+                var _this = this;
+                if(this.checkall){
+                    //实现反选
+                    this.checked=[];
+                } else {
+                    //实现全选
+                    this.checked = [];
+                    this.list.forEach(function (item) {
+                        _this.checked.push(item.id);
+                    });
+                }
+                if(this.checked.length === this.list.length){
+                    this.checkall = true;
+                }
+            }
     }
 });
